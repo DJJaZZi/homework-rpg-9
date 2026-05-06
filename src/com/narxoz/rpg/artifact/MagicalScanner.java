871e2f7f6f6ddc7a752d162a280ed09594c67c0a
DJJaZZi <siganak18@gmail.com>
2026-05-07 01:22:12 +0500

    MagicalScanner created

---
+++ src/com/narxoz/rpg/artifact/MagicalScanner.java
@@ -0,0 +1,29 @@
+package com.narxoz.rpg.artifact;
+
+public class MagicalScanner implements ArtifactVisitor {
+
+    @Override
+    public void visit(Weapon weapon) {
+        System.out.println("⚔ [Weapon] " + weapon.getName() + " (Attack bonus: +" + weapon.getAttackBonus() + ")");
+    }
+
+    @Override
+    public void visit(Potion potion) {
+        System.out.println(" [Potion] " + potion.getName() + " (Restore HP: " + potion.getHealing() + ")");
+    }
+
+    @Override
+    public void visit(Scroll scroll) {
+        System.out.println(" [Scroll] " + scroll.getName() + " (Spell: " + scroll.getSpellName() + ")");
+    }
+
+    @Override
+    public void visit(Ring ring) {
+        System.out.println(" [Ring] " + ring.getName() + " (Magic bonus: +" + ring.getMagicBonus() + ")");
+    }
+
+    @Override
+    public void visit(Armor armor) {
+        System.out.println(" [Armor] " + armor.getName() + " (Defense bonus: +" + armor.getDefenseBonus() + ")");
+    }
+}
